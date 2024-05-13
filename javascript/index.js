b3238ba0cd5e18727b1d7170350b57b983f15d44
Mikko Reini <mikkelsson2006@gmail.com>
2024-05-13 13:56:57 +0300

    ohje sivu tehty

--- javascript/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var instructions = document.getElementById("instructions");
-    instructions.style.display = "none";
-  });
-  function showInstructions() {
-    var instructions = document.getElementById("instructions");
-    instructions.style.display = instructions.style.display === "none" ? "block" : "none";
-  }
